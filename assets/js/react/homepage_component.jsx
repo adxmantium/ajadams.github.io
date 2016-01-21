1d9de89ac03715c8f69792e883b7b05d13fd88c1
Adam Adams <ajadams@gmail.com>
2016-01-20 23:09:35 -0800

    setting up rotating clock hands functions

--- assets/js/react/homepage_component.jsx
+++ assets/js/react/homepage_component.jsx
@@ -1,5 +1,28 @@
 
 var Clock_Component = React.createClass({
+	getInitialState: function(){
+		return {
+
+		};
+	},
+
+	runClock: function(){
+		var sec = $('.sec'),
+			min = $('.min'),
+			hr = $('.hr'),
+			rotate_sec = 0,
+			rotate_min = 90,
+			rotate_hr = 180;
+
+		setInterval(function(){
+			sec.rotate(rotate_sec);
+			rotate_sec += 6;
+
+			if( rotate_sec === 360 )
+				rotate_sec = 0;
+		}, 1000);
+	},
+
 	render: function(){
 		return (
 			<div className="clock">
